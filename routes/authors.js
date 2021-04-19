1c770723e9d57a72a935bc6f8d765ef9b4d93c53
Marian <marianbaba16@gmail.com>
2021-04-19 16:41:47 +0200

    update

--- routes/authors.js
+++ routes/authors.js
@@ -7,13 +7,13 @@ router.get("/", (req, res) => {
 })
 
 // New author route
-routes.get("/new", (req, res) => {
-    res.render("authors/new")
+router.get("/new", (req, res) => {
+    res.render("authors/new");
 })
 
 // Create author route
-routes.post("/", (req, res) => {
-    res.send("Create")
+router.post("/", (req, res) => {
+    res.send("Create");
 })
 
 module.exports = router;
